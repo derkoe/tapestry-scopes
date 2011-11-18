4d374c1ab9d5e1522a7461f45836105331912da3
derkoe <christian.koeberl@gmail.com>
2011-11-18 19:46:41 +0100

    added ScopesModule

--- src/test/java/org/apache/tapestry/scopes/services/AppModule.java
+++ src/test/java/org/apache/tapestry/scopes/services/AppModule.java
@@ -1,8 +1,11 @@
 package org.apache.tapestry.scopes.services;
 
+import org.apache.tapestry.scopes.ScopesModule;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.annotations.SubModule;
 
+@SubModule(ScopesModule.class)
 public class AppModule
 {
     public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration)
